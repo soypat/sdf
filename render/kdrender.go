361f63c8a45250ae61ec76c9596c0437c68975d7
soypat <graded.sp@gmail.com>
2022-05-09 19:40:13 -0300

    keep adding KDSDF

--- render/kdrender.go
+++ render/kdrender.go
@@ -1,11 +1,80 @@
 package render
 
 import (
+	"math"
+
+	"github.com/soypat/sdf"
+	"github.com/soypat/sdf/internal/d3"
 	"gonum.org/v1/gonum/spatial/kdtree"
 	"gonum.org/v1/gonum/spatial/r3"
 )
 
-var _ kdtree.Interface = kdTriangles{}
+var (
+	_ sdf.SDF3         = kdSDF{}
+	_ kdtree.Interface = kdTriangles{}
+)
+
+func NewKDSDF(model []Triangle3) sdf.SDF3 {
+	mykd := make(kdTriangles, len(model))
+	var min, max r3.Vec
+	for i := range mykd {
+		tri := kdTriangle(model[i])
+		mykd[i] = tri
+		triMin := d3.MinElem(tri.V[2], d3.MinElem(tri.V[0], tri.V[1]))
+		triMax := d3.MaxElem(tri.V[2], d3.MaxElem(tri.V[0], tri.V[1]))
+		min = d3.MinElem(triMin, min)
+		max = d3.MaxElem(triMax, max)
+	}
+	tree := kdtree.New(mykd, false)
+	tree.Root.Bounding = &kdtree.Bounding{
+		Min: kdTriangle{V: [3]r3.Vec{min, min, min}},
+		Max: kdTriangle{V: [3]r3.Vec{max, max, max}},
+	}
+	return kdSDF{
+		tree: *tree,
+	}
+}
+
+type kdSDF struct {
+	tree kdtree.Tree
+}
+
+func (s kdSDF) Evaluate(v r3.Vec) float64 {
+	// do some ad-hoc math with the triangle normal ????
+	triangle := s.Nearest(v)
+	minDist := math.MaxFloat64
+	for i := 0; i < 3; i++ {
+		minDist = math.Min(minDist, r3.Norm(r3.Sub(v, triangle.V[i])))
+	}
+	c := kdCentroid(triangle)
+	pointDir := r3.Sub(v, c)
+	n := triangle.Normal()
+	// return math.Copysign(minDist, math.Pi/2-alpha)
+	alpha := math.Acos(r3.Cos(n, pointDir))
+	return math.Copysign(minDist, math.Pi/2-alpha)
+}
+
+// Get nearest triangle to point.
+func (s kdSDF) Nearest(v r3.Vec) kdTriangle {
+	got, _ := s.tree.Nearest(kdTriangle{
+		V: [3]r3.Vec{v, v, v},
+	})
+	// do some ad-hoc math with the triangle normal ????
+	return got.(kdTriangle)
+}
+
+func (s kdSDF) BoundingBox() d3.Box {
+	bb := s.tree.Root.Bounding
+	if bb == nil {
+		panic("got nil bounding box?")
+	}
+	tMin := bb.Min.(kdTriangle)
+	tMax := bb.Max.(kdTriangle)
+	return d3.Box{
+		Min: d3.MinElem(tMin.V[2], d3.MinElem(tMin.V[0], tMin.V[1])),
+		Max: d3.MaxElem(tMax.V[2], d3.MaxElem(tMax.V[0], tMax.V[1])),
+	}
+}
 
 type kdTriangles []kdTriangle
 
@@ -51,6 +120,11 @@ func (a kdTriangle) Distance(b kdtree.Comparable) float64 {
 	return kdDist(a, b.(kdTriangle))
 }
 
+func (a kdTriangle) Normal() r3.Vec {
+	v := Triangle3(a)
+	return v.Normal()
+}
+
 // c = a.dim - b.dim
 func kdComp(a, b kdTriangle, dim int) (c float64) {
 	switch dim {
@@ -72,11 +146,12 @@ func kdDist(a, b kdTriangle) (c float64) {
 }
 
 func kdCentroid(a kdTriangle) r3.Vec {
-	return r3.Vec{
+	v := r3.Vec{
 		X: a.V[0].X + a.V[1].X + a.V[2].X,
 		Y: a.V[0].Y + a.V[1].Y + a.V[2].Y,
 		Z: a.V[0].Z + a.V[1].Z + a.V[2].Z,
 	}
+	return r3.Scale(1./3., v)
 }
 
 type kdPlane struct {
--- render/kdrender_test.go
+++ render/kdrender_test.go
@@ -1,30 +1,38 @@
-package render
+package render_test
 
 import (
 	"testing"
-	"time"
 
 	"github.com/soypat/sdf/form3"
-	"gonum.org/v1/gonum/spatial/kdtree"
+	"github.com/soypat/sdf/internal/d3"
+	"github.com/soypat/sdf/render"
 	"gonum.org/v1/gonum/spatial/r3"
 )
 
-func TestKDLookup(t *testing.T) {
-	s, _ := form3.Sphere(1)
-	model, _ := RenderAll(NewOctreeRenderer(s, 20))
-	mykd := make(kdTriangles, len(model))
-	for i := range mykd {
-		mykd[i] = kdTriangle(model[i])
+func TestKDSDF(t *testing.T) {
+	var defaultView = viewConfig{
+		up:     r3.Vec{Z: 1},
+		eyepos: d3.Elem(3),
+		near:   1,
+		far:    10,
 	}
-	v := kdtree.New(mykd, true)
-	start := time.Now()
-	out, d := v.Nearest(kdTriangle{
-		V: [3]r3.Vec{
-			{1, 0, 0},
-			{1, 0, 0},
-			{1, 0, 0},
-		},
-	})
-	result := out.(kdTriangle)
-	t.Log(len(model), time.Since(start), result, d)
+	const quality = 20
+	s, _ := form3.Sphere(1)
+	err := render.CreateSTL("kd_before.stl", render.NewOctreeRenderer(s, quality))
+	if err != nil {
+		t.Fatal(err)
+	}
+	stlToPNG(t, "kd_before.stl", "kd_before.png", defaultView)
+	model, err := render.RenderAll(render.NewOctreeRenderer(s, quality))
+	if err != nil {
+		t.Fatal(err)
+	}
+	sdf := render.NewKDSDF(model)
+	t.Error(sdf.BoundingBox())
+	outside := sdf.Evaluate(r3.Vec{2, 0, 0}) // evaluate point outside bounds
+	inside := sdf.Evaluate(r3.Vec{0, 0, 0})  // evaluate point inside bounds
+	surface := sdf.Evaluate(r3.Vec{1, 0, 0}) // evaluate point on surface
+	t.Errorf("outside:%.2g, inside:%.2g, surface:%.2g", outside, inside, surface)
+	render.CreateSTL("kd_after.stl", render.NewOctreeRenderer(sdf, quality))
+	stlToPNG(t, "kd_after.stl", "kd_after.png", defaultView)
 }
